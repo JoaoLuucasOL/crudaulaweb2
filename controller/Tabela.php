793c2a8f4eb9008c1a04d20b902bb2297fb6b045
lazarodu <lazarodu@gmail.com>
2022-05-03 08:22:39 -0300

    Aula do dia 03/05/2022

--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,30 +2,47 @@
 class Tabela
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
   }
   public function controller()
   {
-    Transaction::get();
-    $computador = new Crud("computador");
-    $resultado = $computador->select();
-    $tabela = new Template("view/tabela.html");
-    if (is_array($resultado)) {
-      $tabela->set("linha", $resultado);
-      $this->message = $tabela->saida();
+    try {
+      Transaction::get();
+      $computador = new Crud("computador");
+      $resultado = $computador->select();
+      $tabela = new Template("view/tabela.html");
+      if (is_array($resultado)) {
+        $tabela->set("linha", $resultado);
+        $this->message = $tabela->saida();
+      } else {
+        $this->message = $computador->getMessage();
+        $this->error = $computador->getError();
+      }
+    } catch (Exception $e) {
+      $this->message = $e->getMessage();
+      $this->error = true;
     }
   }
   public function remover()
   {
-    try {
-      $conexao = Transaction::get();
-      $id = $conexao->quote($_GET["id"]);
-      $computador = new Crud("computador");
-      $resultado = $computador->delete("id = $id");
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $computador = new Crud("computador");
+        $computador->delete("id = $id");
+        $this->message = $computador->getMessage();
+        $this->error = $computador->getError();
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    } else {
+      $this->message = "Faltando parâmetro!";
+      $this->error = true;
     }
   }
   public function getMessage()
